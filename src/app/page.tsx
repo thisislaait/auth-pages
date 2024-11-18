2726de8a96bfdaa5bad938bd2db315d2d8b8fc04
thisislaait <ogadi.ugo@gmail.com>
2024-11-18 22:37:41 +0100

    create button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
+'use client';
+
 import Image from "next/image";
+import Button from "./components/Button";
+import LinkButton from "./components/Link";
+import TextLink from "./components/TextLink";
 
 export default function Home() {
   return (
@@ -23,22 +28,21 @@ export default function Home() {
           <li>Save and see your changes instantly.</li>
         </ol>
 
+        
+        <Button
+          label="Click Me"
+          onClick={() => alert("Button Clicked!")}
+          className="text-sm mt-8" // Optional styling for spacing
+        />
+
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+          <LinkButton
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
+            label="Deploy now"
+            imageSrc="/vercel.svg"
+            imageAlt="Vercel logomark"
+            className="text-sm sm:text-base"
+          />
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -47,8 +51,17 @@ export default function Home() {
           >
             Read our docs
           </a>
+
+          <TextLink
+            href="/login"
+            label="Already have an account? Login"
+            className="mt-4"
+          />
         </div>
+
       </main>
+
+
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
